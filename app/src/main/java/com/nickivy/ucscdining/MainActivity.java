8b7bb778e5cb6a42b38831afeccf4903f7631c95
Nicky Ivy <parkedraccoon@gmail.com>
2015-04-28 23:32:22 -0700

    Android Studio migration

---
+++ app/src/main/java/com/nickivy/ucscdining/MainActivity.java
@@ -0,0 +1,122 @@
+package com.nickivy.ucscdining;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.nickivy.ucscdining.R;
+import com.nickivy.ucscdining.MealViewFragment;
+import com.nickivy.ucscdining.parser.MenuParser;
+
+/**
+ * App for viewing UCSC dining menus. Currently can 
+ * read all menus, display them based on time, with special colors displayed 
+ * for events such as College Nights, Healthy Mondays, or Farm Fridays.
+ * 
+ * <p>Will eventually allow the user to fast forward to see planned menus for
+ * days in the future.
+ * 
+ * <p>TODO: smart refresh
+ * <p>TODO: tablet layout (display all 3 meals at once)
+ *
+ * @author Nick Ivy parkedraccoon@gmail.com
+ */
+
+public class MainActivity extends ActionBarActivity{
+	
+	static final int ITEMS = 3;
+	
+	private ListView mDrawerList;
+	private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ActionBar mActionBar;
+    private MealViewFragment fragment;
+    
+    private static int currentCollege = 0;
+	
+    @Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		
+		setContentView(R.layout.mainview);
+		
+//		if(findViewById(R.id.fragment_container) != null) {
+			
+/*			if (savedInstanceState != null) {
+				return;
+			}*/
+			
+			mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+	        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+	        mActionBar = getSupportActionBar();
+
+	        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+			mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+					R.layout.drawer_list_item, MenuParser.collegeList));
+			
+	        mActionBar.setDisplayHomeAsUpEnabled(true);
+	        mActionBar.setHomeButtonEnabled(true);
+	        
+	        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+	        		R.string.drawer_open, R.string.drawer_close);
+	        
+	        mDrawerLayout.setDrawerListener(mDrawerToggle);
+	        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+	        
+	        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+	        fragment = new MealViewFragment();
+	        Bundle args = new Bundle();
+	        args.putInt(MealViewFragment.ARG_COLLEGE_NUMBER, currentCollege);
+	        fragment.setArguments(args);
+	        transaction.replace(R.id.fragment_container, fragment);
+	        transaction.commit();
+	        
+/*	        if (savedInstanceState == null) {
+//	            selectItem(0);
+	        	fragment.selectItem(0);
+	        }*/
+//		}
+		
+	}
+    
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            selectItem(position);
+        	fragment.selectItem(position);
+        }
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       if (mDrawerToggle.onOptionsItemSelected(item)) {
+           return true;
+       }
+       return super.onOptionsItemSelected(item);
+   }
+
+}
---
+++ app/src/main/java/com/nickivy/ucscdining/util/CollegeMenu.java
@@ -0,0 +1,131 @@
+package com.nickivy.ucscdining.util;
+
+import java.util.ArrayList;
+
+import android.util.Log;
+
+/**
+ * College menu object. Keeps things organized,
+ * as I would end up using 3-dimensional arraylists otherwise.
+ *
+ * @author Nick Ivy parkedraccoon@gmail.com
+ */
+
+public class CollegeMenu {
+	
+	private ArrayList<String> mBreakfast = new ArrayList<String>();
+	private ArrayList<String> mLunch = new ArrayList<String>();
+	private ArrayList<String> mDinner = new ArrayList<String>();
+	
+	private boolean isSet = false;
+	
+	public CollegeMenu(){}
+	
+	public CollegeMenu(ArrayList<String> breakfast, ArrayList<String> lunch, ArrayList<String> dinner){
+		mBreakfast = breakfast;
+		mLunch = lunch;
+		mDinner = dinner;
+	}
+	
+	public void setBreakfast(ArrayList<String> meal){
+		mBreakfast = meal;
+		isSet = true;
+	}
+	
+	public void setLunch(ArrayList<String> meal){
+		mLunch = meal;
+		isSet = true;
+	}
+	
+	public void setDinner(ArrayList<String> meal){
+		mDinner = meal;
+		isSet = true;
+	}
+	
+	public ArrayList<String> getBreakfast(){
+		if(mBreakfast != null && mBreakfast.size() > 0){
+			return mBreakfast;
+		}
+		ArrayList<String> ret = new ArrayList<String>();
+		return ret;
+	}
+	
+	public ArrayList<String> getLunch(){
+		if(mLunch != null && mLunch.size() > 0){
+			return mLunch;
+		}
+		ArrayList<String> ret = new ArrayList<String>();
+		return ret;
+	}
+	
+	public ArrayList<String> getDinner(){
+		if(mDinner != null && mDinner.size() > 0){
+			return mDinner;
+		}
+		ArrayList<String> ret = new ArrayList<String>();
+		return ret;
+	}
+	
+	/**
+	 * Get if data has been written to the 
+	 * object at all
+	 * @return
+	 */
+	public boolean getIsSet(){
+		return isSet;
+	}
+	
+	
+	/**
+	 * Get if the dining hall is open
+	 * 
+	 * <p>At this point assume open if nothing has been loaded
+	 * @return
+	 */
+	public boolean getIsOpen(){
+    	if(isSet){
+    		if(getBreakfast().isEmpty() && 	getLunch().isEmpty() && getDinner().isEmpty()){
+    			return false;
+    		}
+    	}
+    	return true;
+	}
+	
+	/**
+	 * If the first entry in Dinner is
+	 * "College Night" then it's a college night	
+	 * @return
+	 */
+	public boolean getIsCollegeNight(){
+    	if(isSet && getDinner().size() > 0 && getDinner().get(0).equals("College Night")){
+    		return true;
+    	}
+    	return false;
+	}
+	
+	/**
+	 * @return If first entry in dinner or lunch is "Healthy Mondays"
+	 */
+	public boolean getIsHealthyMonday(){
+    	if(isSet){
+    		if((getDinner().size() > 0 && getDinner().get(0).equals("Healthy Mondays")) || 
+    			getLunch().size() > 0 && getLunch().get(0).equals("Healthy Mondays")){    			
+    			return true;
+    		}
+    	}
+    	return false;		
+	}
+
+	/**
+	 * @return If first entry in dinner or lunch is "Farm Fridays"
+	 */
+	public boolean getIsFarmFriday(){
+    	if(isSet){
+    		if((getDinner().size() > 0 && getDinner().get(0).equals("Farm Fridays")) || 
+    			getLunch().size() > 0 && getLunch().get(0).equals("Farm Fridays")){    			
+    			return true;
+    		}
+    	}
+    	return false;		
+	}
+}
